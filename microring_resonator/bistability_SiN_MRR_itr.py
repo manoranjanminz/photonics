6abc70ba4d437d6a422c01ed2397f103e19f7514
manoranjanminz <102401390+manoranjanminz@users.noreply.github.com>
2024-08-16 18:25:29 +0530

    Update bistability_SiN_MRR_itr.py

--- microring_resonator/bistability_SiN_MRR_itr.py
+++ microring_resonator/bistability_SiN_MRR_itr.py
@@ -20,40 +20,31 @@ Q_l = 12700
 lambda_0 = 1550e-9
 
 # Initial values
-delta_lambda_therm = 0
-delta_lambda_kerr = 0
-omega_0 = 2 * np.pi * c / (lambda_0 + delta_lambda_therm + delta_lambda_kerr)
 P_in = np.array([0.001, 0.01, 0.1, 1, 5]) # in mW
-tau_in = Q_in / omega_0
-tau_v = Q_v / omega_0
-
-omega = np.linspace(2 * np.pi * 193.62e12, 2 * np.pi * 193.20e12, 2000)
+omega = np.linspace(2 * np.pi * 193.62e12, 2 * np.pi * 193.20e12, 200)
 # omega_rev = np.linspace(2 * np.pi * 193.20e12, 2 * np.pi * 193.62e12, 2000)
 photon_energy = np.zeros((len(P_in), len(omega)))
 
+# fixed omega
 fixed_omega = 2 * np.pi * c / (lambda_0 + 0.1e-9)
-delta_lambda_therm_fo = 0
-delta_lambda_kerr_fo = 0
-omega_0_fo = 2 * np.pi * c / (lambda_0 + delta_lambda_therm_fo + delta_lambda_kerr_fo)
-tau_in_fo = tau_in
-tau_v_fo = tau_v
 P_in_fo = np.linspace(1,15,200)
 photon_energy_fixed_omega = np.zeros((len(P_in_fo), 1))
 
-delta_lambda_therm_fo_rev = 0
-delta_lambda_kerr_fo_rev = 0
-omega_0_fo_rev = 2 * np.pi * c / (lambda_0 + delta_lambda_therm_fo + delta_lambda_kerr_fo)
-tau_in_fo_rev = tau_in
-tau_v_fo_rev = tau_v
+# fixed omega and power in reverse order
 P_in_fo_rev = np.linspace(15,1,200)
 photon_energy_fixed_omega_rev = np.zeros((len(P_in_fo_rev), 1))
 
-n_itr = 20 # No. of iterations for a fixed omega
+n_itr = 1000 # No. of iterations for a fixed omega
 
 photon_energy_evo = np.zeros((len(P_in), len(omega), n_itr))
 x_n_itr = np.linspace(1,n_itr,n_itr)
 # Nonlinear CMT model
 for i in range(len(P_in)):
+    delta_lambda_therm = 0
+    delta_lambda_kerr = 0
+    omega_0 = 2 * np.pi * c / (lambda_0 + delta_lambda_therm + delta_lambda_kerr)
+    tau_in = Q_in / omega_0
+    tau_v = Q_v / omega_0
     for j in range(len(omega)):
         for z in range(n_itr):
             photon_energy[i, j] = ((1 / tau_in) * (P_in[i] / 2)) / ((omega[j] - omega_0) ** 2 + (((1 / tau_in) + (1 / tau_v) + (1 / tau_linear)) ** 2) / 4)
@@ -67,6 +58,11 @@ for i in range(len(P_in)):
 
 # Cavity Energy vs Input Power for a fixed omega
 for i in range(len(P_in_fo)):
+    delta_lambda_therm_fo = 0
+    delta_lambda_kerr_fo = 0
+    omega_0_fo = 2 * np.pi * c / (lambda_0 + delta_lambda_therm_fo + delta_lambda_kerr_fo)
+    tau_in_fo = Q_in/omega_0_fo
+    tau_v_fo = Q_v/omega_0_fo
     for z in range(n_itr):
         photon_energy_fixed_omega[i] = ((1 / tau_in_fo) * (P_in_fo[i] / 2)) / ((fixed_omega - omega_0_fo) ** 2 + (((1 / tau_in_fo) + (1 / tau_v_fo) + (1 / tau_linear)) ** 2) / 4)
         
@@ -78,6 +74,11 @@ for i in range(len(P_in_fo)):
 
 
 for i in range(len(P_in_fo_rev)):
+    delta_lambda_therm_fo_rev = 0
+    delta_lambda_kerr_fo_rev = 0
+    omega_0_fo_rev = 2 * np.pi * c / (lambda_0 + delta_lambda_therm_fo + delta_lambda_kerr_fo)
+    tau_in_fo_rev = Q_in/omega_0_fo_rev
+    tau_v_fo_rev = Q_v/omega_0_fo_rev
     for z in range(n_itr):
         photon_energy_fixed_omega_rev[i] = ((1 / tau_in_fo_rev) * (P_in_fo_rev[i] / 2)) / ((fixed_omega - omega_0_fo_rev) ** 2 + (((1 / tau_in_fo_rev) + (1 / tau_v_fo_rev) + (1 / tau_linear)) ** 2) / 4)
         
@@ -110,7 +111,7 @@ plt.xlim([min(x_n_itr), max(x_n_itr)])
 plt.legend(loc='lower right', fontsize=6)
 plt.show()
 
-delta_omega_loc = len(omega)/32
+delta_omega_loc = len(omega)/40
 print('round(len(omega)/2 + len(omega/4)):: ',round(len(omega)/2 + delta_omega_loc))
 print('round(len(omega)/2 - len(omega/4)):: ',round(len(omega)/2 - delta_omega_loc))
 
